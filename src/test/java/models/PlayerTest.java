cdf7e16d3773593e95dd66fbca244540893c61f0
Kristen <peanutster@gmail.com>
2018-07-18 14:14:23 -0700

    test getter methods for player

--- src/test/java/models/PlayerTest.java
+++ src/test/java/models/PlayerTest.java
@@ -13,27 +13,37 @@ public class PlayerTest {
     }
 
     @Test
-    public void getFirstName() {
+    public void newPlayerInstantiatesWithFirstName() {
+        Player testPlayer = setupPlayer();
+        assertEquals("Kristen", testPlayer.getFirstName());
     }
 
     @Test
-    public void getLastName() {
+    public void newPlayerInstantiatesWithLastName() {
+        Player testPlayer = setupPlayer();
+        assertEquals("Chellis", testPlayer.getLastName());
     }
 
     @Test
-    public void getEmail() {
+    public void newPlayerInstantiatesWithEmail() {
+        Player testPlayer = setupPlayer();
+        assertEquals("peanutster@gmail.com", testPlayer.getEmail());
     }
 
     @Test
-    public void getShirtSize() {
+    public void newPlayerInstantiatesWithShirtSize() {
+        Player testPlayer = setupPlayer();
+        assertEquals("Small", testPlayer.getShirtSize());
     }
 
     @Test
-    public void getGender() {
+    public void newPlayerInstantiatesWithGender() {
+        Player testPlayer = setupPlayer();
+        assertEquals("female", testPlayer.getGender());
     }
 
     public Player setupPlayer(){
-        Player samplePlayer = new Player("Kristen", "Chellis", "peanutster@gmail.com", "small", "female");
+        Player samplePlayer = new Player("Kristen", "Chellis", "peanutster@gmail.com", "Small", "female");
         return samplePlayer;
     }
 }
