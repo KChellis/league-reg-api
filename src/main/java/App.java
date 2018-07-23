3b4bd7aa93cbdbc9ff93a4ac7a10e9f47a99b9f4
Kristen <k.n.chellis@gmail.com>
2018-07-23 16:13:36 -0700

    add routes for gameDao functions

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,12 +1,6 @@
 import com.google.gson.Gson;
-import dao.Sql2oLeagueDao;
-import dao.Sql2oPlayerDao;
-import dao.Sql2oSportDao;
-import dao.Sql2oTeamDao;
-import models.League;
-import models.Player;
-import models.Sport;
-import models.Team;
+import dao.*;
+import models.*;
 import org.sql2o.Connection;
 import org.sql2o.Sql2o;
 
@@ -17,6 +11,7 @@ import static spark.Spark.*;
 
 public class App {
     public static void main(String[] args) {
+        Sql2oGameDao gameDao;
         Sql2oLeagueDao leagueDao;
         Sql2oPlayerDao playerDao;
         Sql2oSportDao sportDao;
@@ -27,11 +22,85 @@ public class App {
         String connectionString = "jdbc:h2:~/league.db;INIT=RUNSCRIPT from 'classpath:db/create.sql'";
         Sql2o sql2o = new Sql2o(connectionString, "", "");
 
+        gameDao = new Sql2oGameDao(sql2o);
         leagueDao = new Sql2oLeagueDao(sql2o);
         playerDao = new Sql2oPlayerDao(sql2o);
         sportDao = new Sql2oSportDao(sql2o);
         teamDao = new Sql2oTeamDao(sql2o);
         conn = sql2o.open();
+
+        get("/games", "application/json", (req, res) -> {
+            List<Game> allGames = gameDao.getAll();
+            if(allGames.size() == 0){
+                res.status(404);
+                return "{\"message\":\"Sorry there are no games currently.\"}";
+            }else{
+                return gson.toJson(allGames);
+            }
+
+        });
+
+        post("/games/new", "application/json", (req, res) -> {
+            try {
+                Game game = gson.fromJson(req.body(), Game.class);
+                gameDao.add(game);
+                res.status(201);
+                return gson.toJson(game);
+            } catch(Error error) {
+                res.status(400);
+                return "{\"message\":\"Sorry your request could not be processed\"}";
+            }
+
+        });
+
+        get("/games/:gameId", "application/json", (req, res) -> {
+            try {
+                int gameId = Integer.parseInt(req.params("gameId"));
+                Game currentGame = gameDao.findById(gameId);
+                if(currentGame != null){
+                    return gson.toJson(currentGame);
+                } else {
+                    res.status(404);
+                    return "{\"message\":\"Game not found\"}";
+                }
+            } catch ( Error error){
+                res.status(400);
+                return "{\"message\":\"Sorry your request could not be processed\"}";
+            }
+        });
+
+        get("/games/:gameId/delete", "application/json", (req, res) -> {
+            try{
+                int gameId = Integer.parseInt(req.params("gameId"));
+                gameDao.deleteById(gameId);
+                return "{\"message\":\"Sorry your request could not be processed\"}";
+            } catch (Error error){
+                res.status(400);
+                return "{\"message\":\"Sorry your request could not be processed\"}";
+            }
+
+        });
+
+        post("/games/:gameId/update", "application/json", (req, res) -> {
+            try{
+                int gameId = Integer.parseInt(req.params("gameId"));
+                Game currentGame = gameDao.findById(gameId);
+                if (currentGame != null){
+                    HashMap<String, Object> updateContent = gson.fromJson(req.body(), HashMap.class);
+                    res.status(201);
+                    gameDao.update(gameId, updateContent);
+                    return gson.toJson(gameDao.findById(gameId));
+                } else{
+                    res.status(404);
+                    return "{\"message\":\"Game not found\"}";
+                }
+
+            } catch (Error error){
+                res.status(400);
+                return "{\"message\":\"Sorry your request could not be processed\"}";
+            }
+
+        });
 
         get("/leagues", "application/json", (req, res) -> {
             List<League> allLeagues = leagueDao.getAll();
@@ -82,6 +151,22 @@ public class App {
                 } else {
                     res.status(404);
                     return "{\"message\":\"No teams match this league\"}";
+                }
+            } catch ( Error error){
+                res.status(400);
+                return "{\"message\":\"Sorry your request could not be processed\"}";
+            }
+        });
+
+        get("/leagues/:leagueId/games", "application/json", (req, res) -> {
+            try {
+                int leagueId = Integer.parseInt(req.params("leagueId"));
+                List<Game> gamesByLeague = gameDao.findByLeague(leagueId);
+                if(gamesByLeague.size() > 0){
+                    return gson.toJson(gamesByLeague);
+                } else {
+                    res.status(404);
+                    return "{\"message\":\"No games match this league\"}";
                 }
             } catch ( Error error){
                 res.status(400);
