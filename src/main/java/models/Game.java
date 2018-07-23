9950e8b95a440c8cfdc12136229ec96c3124a523
Kristen <k.n.chellis@gmail.com>
2018-07-23 15:02:41 -0700

    write game model and test its getters and setters

---
+++ src/main/java/models/Game.java
@@ -0,0 +1,7 @@
+package models;
+
+/**
+ * Created by Guest on 7/23/18.
+ */
+public class Game {
+}
---
+++ src/test/java/models/GameTest.java
@@ -0,0 +1,8 @@
+import static org.junit.Assert.*;
+
+/**
+ * Created by Guest on 7/23/18.
+ */
+public class GameTest {
+
+}
